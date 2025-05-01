53a793b3766793742ce54af15a76786f9bf3df0a
myra2004 <amirovamuqaddas@gmail.com>
2025-05-01 20:00:08 +0500

    added template

--- posts/urls.py
+++ posts/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from .views import HomePageView
+from posts import views
 
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('citizen_kane/', views.citizen_kane),
+    path('casablanca/', views.casablanca),
+    # path('maltese_falcon/', views.maltese_falcon),
+    path('psycho/', views.psycho),
 ]
 
